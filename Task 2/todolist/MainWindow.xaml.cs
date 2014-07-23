f25b93f41a7ee0712d26e1ded3a200d1f2491c1b
Ahmedphjt <bahgat_cs@yahoo.com>
2014-07-22 23:55:34 -0700

    Tasks
    Just i named the previous task with "Task 2 "
    and the Currently task with "Task 3 "
    the Task 3 is Completed except Color handling

---
+++ Task 2/todolist/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Annotations;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Converters;
+using System.Windows.Automation;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+
+
+namespace todolist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (NewTodo.Text == String.Empty)
+            {
+                MessageBox.Show("Helpful message !! ");
+            }
+
+            else
+            {
+                String input = NewTodo.Text;
+                CheckBox checkbox = new CheckBox();
+                checkbox.Content = input;
+                List.Items.Add(checkbox);
+            }      
+
+
+            
+            
+
+        }
+
+
+    }
+}
