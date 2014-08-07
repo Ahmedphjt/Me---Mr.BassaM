595039aa872d3bdf8b14ce3426839553a71131eb
Ahmedphjt <bahgat_cs@yahoo.com>
2014-08-07 00:23:21 -0700

    Task 4
    Tutorial of task 4 which include tree view of the 3 priorities

---
+++ Task 4 tutorial/todolist/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Annotations;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Converters;
+using System.Windows.Automation;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+
+
+namespace todolist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen; //   start the form in the middle os screen 
+        }
+
+        protected void ButtonClick(object sender, RoutedEventArgs e)
+        {
+
+
+            CheckBox checkbox = new CheckBox();
+            String input;
+            // is the the user enter an empty text or white space 
+            string N = NewTodo.Text;
+            if (String.IsNullOrWhiteSpace(N))
+            {
+                MessageBox.Show("Please Enter a Valid Text Name , Not empty text ");
+            }
+                // handle priority 
+            else if (Compo.SelectedItem==null)
+            {
+                MessageBox.Show("you Must Select an item form Compolist ");
+
+            }
+
+            else
+            {
+                 input = NewTodo.Text;
+                 if (Compo.Text == "Low")
+                 {
+                     low.Items.Add(input);  
+                 }
+                 else if (Compo.Text == "Medium")
+                 {
+                     med.Items.Add(input);
+                 }
+                 else
+                 {
+                     high.Items.Add(input);
+                 }
+                 NewTodo.Clear();
+
+
+            }
+            if (Compo.Text == "Low")
+            {
+                checkbox.Foreground = Brushes.YellowGreen;
+                
+            }
+
+            else if (Compo.Text == "Medium") 
+            {
+                checkbox.Foreground = Brushes.Blue;
+            }
+
+            else
+            {
+                checkbox.Foreground = Brushes.Red;
+            }
+
+             
+
+        }
+
+    }
+}
